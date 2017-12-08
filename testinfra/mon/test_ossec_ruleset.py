e2eb1c662ee6e4c204b09bbc48eefeeb88c3ed62
Mickael E <mickael@freedom.press>
2017-12-08 00:06:29 -0500

    Override OSSEC alerts 502 and 503 to prevent email the sending of email alerts. Alerts are not fully supressed (i.e. logged to disk) because they could provide useful debug information (e.g. ensure that the server correctly reboots nightly)

--- testinfra/mon/test_ossec_ruleset.py
+++ testinfra/mon/test_ossec_ruleset.py
@@ -4,6 +4,7 @@ import re
 
 alert_level_regex = re.compile(r"Level: '(\d+)'")
 rule_id_regex = re.compile(r"Rule id: '(\d+)'")
+email_suppressed_regex = re.compile(r"no_email_alert")
 sdvars = pytest.securedrop_test_vars
 
 
@@ -27,3 +28,18 @@ def test_ossec_expected_alerts_are_present(Command, Sudo, log_event):
         assert alert_level == log_event["level"]
         rule_id = rule_id_regex.findall(c.stderr)[0]
         assert rule_id == log_event["rule_id"]
+
+
+@pytest.mark.parametrize('log_event',
+                         sdvars.log_events_with_ossec_alerts)
+def test_ossec_emails_supressed_as_expected(Command, Sudo, log_event):
+    with Sudo():
+        c = Command('echo "{}" | /var/ossec/bin/ossec-logtest'.format(
+                 log_event["alert"]))
+        assert "Alert to be generated" in c.stderr
+        alert_level = alert_level_regex.findall(c.stderr)[0]
+        assert alert_level == log_event["level"]
+        rule_id = rule_id_regex.findall(c.stderr)[0]
+        assert rule_id == log_event["rule_id"]
+        email_suppressed = email_suppressed_regex.findall(c.stderr)[0]
+        assert email_suppressed == log_event["options"]
